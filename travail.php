6ff21124896cb7d44bdc555de404e0aaf2730406
Moutima <113523286+Moutima@users.noreply.github.com>
2023-03-06 13:52:27 +0100

    Add files via upload

---
+++ travail.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<h1>Calcul du factoriel</h1>
+<script>
+    function calculerfactoriel () {
+        let n = prompt ("entre un chiffre N :");
+        let factoriel = 1;
+        for (let i=1; 1 <= n; 1++) {
+            factoriel = i;
+        
+        
+        }
+        
+        alerte("le factoriel de" + n+ "est" + factoriel);
+    }
+</script> 
+
+<h1>Calcul du factoriel</h1>
+<button onclick"calculerfactoriel()">calculer le factoriel</button>
+
+
+</body>
+</html>
